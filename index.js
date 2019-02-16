2ffc97ac5d08b8d0cf781f9709ecf10b9830f3f9
ritvik1512 <ritvik@outlook.com>
2019-02-16 14:25:03 +0900

    bug testing by adding random new function

--- index.js
+++ index.js
@@ -31,7 +31,45 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {
+
+app.post('/webhook/', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+
+
+
+
+
+
+
+/*app.post('/webhook', (req, res) => {
 
   // Return a '200 OK' response to all events
   res.status(200).send('EVENT_RECEIVED');
@@ -65,7 +103,7 @@ app.post('/webhook', (req, res) => {
         });
       });
     }
-});
+});*/
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
